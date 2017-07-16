5a9799cc4e272f95fe9193d27f5ce0789e455cfb
liumy46 <liumy46@ziroom.com>
2017-07-16 16:14:16 +0800

    sad

--- src/main/java/com/eric/demo/Application.java
+++ src/main/java/com/eric/demo/Application.java
@@ -2,12 +2,32 @@ package com.eric.demo;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.*;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-@Configuration
 @ComponentScan
-@EnableAutoConfiguration
+@Configuration
+@Import({
+		DispatcherServletAutoConfiguration.class,
+		EmbeddedServletContainerAutoConfiguration.class,
+		ErrorMvcAutoConfiguration.class,
+		HttpEncodingAutoConfiguration.class,
+		HttpMessageConvertersAutoConfiguration.class,
+		JacksonAutoConfiguration.class,
+		JmxAutoConfiguration.class,
+		MultipartAutoConfiguration.class,
+		ServerPropertiesAutoConfiguration.class,
+		PropertyPlaceholderAutoConfiguration.class,
+		ThymeleafAutoConfiguration.class,
+		WebMvcAutoConfiguration.class,
+		WebSocketAutoConfiguration.class,
+})
 public class Application {
 	
 	public static void main(String[] args) throws Exception {
