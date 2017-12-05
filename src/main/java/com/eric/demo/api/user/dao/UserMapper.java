89a1e49292fbb5a7481f85d872af3d9c368ffa2a
liumingyue <liumingyue@Ctrip.com>
2017-12-05 19:55:06 +0800

    日志更改

--- src/main/java/com/eric/demo/api/user/dao/UserMapper.java
+++ src/main/java/com/eric/demo/api/user/dao/UserMapper.java
@@ -9,27 +9,5 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
 public interface UserMapper extends BaseDao<User,UserCriteria,String>{
-    int countByExample(UserCriteria example);
 
-    int deleteByExample(UserCriteria example);
-
-    int deleteByPrimaryKey(String id);
-
-    int insert(User record);
-
-    int insertSelective(User record);
-
-    List<User> selectByExampleWithRowbounds(UserCriteria example, RowBounds rowBounds);
-
-    List<User> selectByExample(UserCriteria example);
-
-    User selectByPrimaryKey(String id);
-
-    int updateByExampleSelective(@Param("record") User record, @Param("example") UserCriteria example);
-
-    int updateByExample(@Param("record") User record, @Param("example") UserCriteria example);
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
 }
