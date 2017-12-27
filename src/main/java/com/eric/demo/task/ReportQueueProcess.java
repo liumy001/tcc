faf4c871fab5b19f831bab51fceb880deb65db4e
liumingyue <liumingyue@Ctrip.com>
2017-12-27 20:10:48 +0800

    添加校验文件

--- src/main/java/com/eric/demo/task/ReportQueueProcess.java
+++ src/main/java/com/eric/demo/task/ReportQueueProcess.java
@@ -2,12 +2,22 @@ package com.eric.demo.task;
 
 import com.alibaba.fastjson.JSON;
 import com.eric.demo.commons.util.Check;
+import com.eric.demo.commons.util.DateUtil;
+import com.eric.demo.commons.validator.BaseConst;
+import com.eric.demo.web.bill.domain.Bill;
 import com.eric.demo.web.bill.domain.BillCriteria;
 import com.eric.demo.web.bill.domain.BillReportTask;
 import com.eric.demo.web.bill.service.IBillService;
+import com.eric.demo.web.users.domain.User;
+import com.eric.demo.web.users.domain.UserCriteria;
+import com.eric.demo.web.users.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
+import javax.mail.internet.MimeMessage;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -22,6 +32,12 @@ public class ReportQueueProcess {
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private UserService userService;
 
     public void execute() throws Exception {
 
@@ -45,9 +61,28 @@ public class ReportQueueProcess {
         }
     }
 
-    private void process(BillReportTask billReportTask) {
+    private void process(BillReportTask billReportTask) throws Exception {
         BillCriteria billCriteria = new BillCriteria();
+        billCriteria.or().andIsDelEqualTo(BaseConst.isDel.no_Del.getCode()).andUidEqualTo(billReportTask.getUid()).andConsumTimeGreaterThanOrEqualTo(billReportTask.getEndTime()).andConsumTimeLessThanOrEqualTo(billReportTask.getStartTime());
+        List<Bill> billList = billService.search(billCriteria);
+        int totalPrice = 0;
+        for (Bill bill : billList) {
+            totalPrice += bill.getAmount();
+        }
 
+        UserCriteria userCriteria = new UserCriteria();
+        userCriteria.or().andIdEqualTo(billReportTask.getUid());
+        List<User> userList = userService.search(userCriteria);
+
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("liumingyue0203@163.com");
+        helper.setTo(userList.get(0).getEmail());
+        helper.setSubject("消费明细");
+        //html 加如参数 true
+        helper.setText("日期："+ DateUtil.dateFormat(billReportTask.getStartTime())+"-"+DateUtil.dateFormat(billReportTask.getEndTime())+"累计消费："+totalPrice/100.0+"元");
+        mailSender.send(message);
     }
 
 }
