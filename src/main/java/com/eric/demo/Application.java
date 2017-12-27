a8153d97f469996ddf4246c7f9674e7c48b78c20
liumingyue <liumingyue@Ctrip.com>
2017-12-27 17:30:35 +0800

    添加校验文件

--- src/main/java/com/eric/demo/Application.java
+++ src/main/java/com/eric/demo/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import java.text.ParseException;
@@ -14,6 +15,7 @@ import java.util.Date;
 @SpringBootApplication
 @EnableAsync
 @EnableRedisHttpSession
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) throws Exception {
